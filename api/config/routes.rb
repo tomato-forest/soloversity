7b57d0cdca2dd4c1c493352f561a75dffd680a8f
YoshimasaAsada <yoshimasa111112@gmail.com>
2022-11-18 20:58:04 +0900

    [add]adminのdevise追加と諸々の修正
    adminのdeviseを追加しました。
    
    ルーティングを変更しました。
    auth/registrationsから
    student/registrationsとadmin/registrationsの２つに変更
    
    studentテーブルから不要なカラムを消去しました。
    nickname、name、imageカラムの削除

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'Student', at: 'auth', controllers: {
-    registrations: 'auth/registrations'
+  mount_devise_token_auth_for 'Student', at: 'student', controllers: {
+    registrations: 'student/registrations'
+  }
+
+  mount_devise_token_auth_for 'Admin', at: 'admin', controllers: {
+    registrations: 'admin/registrations'
   }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

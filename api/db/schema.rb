190f0e96f6faba1a29a66cc42477079b91859e87
yutomizuta <yuto11752@i.softbank.jp>
2022-12-07 14:34:55 +0900

    [add]StudentProfile_controller&model

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_18_110207) do
+ActiveRecord::Schema.define(version: 2022_12_07_043321) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,6 +63,21 @@ ActiveRecord::Schema.define(version: 2022_11_18_110207) do
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
     t.index ["uid", "provider"], name: "index_admins_on_uid_and_provider", unique: true
+  end
+
+  create_table "student_profiles", force: :cascade do |t|
+    t.bigint "student_id", null: false
+    t.string "last_name", null: false
+    t.string "first_name", null: false
+    t.integer "gender", null: false
+    t.integer "birthplace", null: false
+    t.string "university_name", null: false
+    t.integer "grade", null: false
+    t.string "faculty", null: false
+    t.string "depertment", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["student_id"], name: "index_student_profiles_on_student_id"
   end
 
   create_table "students", force: :cascade do |t|

00c963bd2bf0679830976b53597813e76923c9e2
yuki-fujisaki <yuki.f.bizz@gmail.com>
2022-12-08 18:36:30 +0900

    [add] headerコンポーネント切り出し

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Header from './header'
 
 const Home: NextPage = () => {
   return (
@@ -11,41 +12,11 @@ const Home: NextPage = () => {
         <meta name="description" content="Soloversity" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-        <header className="right-0 left-0 top-0 z-50 mx-auto flex w-full items-center justify-between p-4 md:h-[120px] md:p-0 md:px-[60px] bg-[#6794A3] text-[#FFFFFF]">
-          <h1 className='text-2xl font-bold'>
-            Soloversity
-          </h1>
-
-          <div className='flex items-center justify-between'>
-          <div className='p-4'>
-            Profile
-          </div>
-          <div className='p-4'>
-            DM
-          </div>
-          <div className='p-4'>
-            ABOUT
-          </div>
-          <div className='p-4'>
-            Public BBS
-          </div>
-          <div className='p-4'>
-            University BBS
-          </div>
-          <div className='p-4'>
-            Registration
-          </div>
-          <div className='p-4'>
-            SignIn
-          </div>
-          </div>
-        </header>
-
+      <Header></Header>
         <h1 className='text-6xl h-80 w-screen flex justify-center items-center fixed'>
             <span>Let’s spread friendship</span>
         </h1>
-        
+
         <main className="right-0 left-0 top-0 z-50 mx-auto flex w-full items-center justify-between p-4 md:h-[460px] md:p-0 md:px-[60px] bg-[url('/top/background-image.jpg')] opacity-25" >
         </main>
 

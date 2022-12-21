6756d722f535f365759ddd30ea3d2d70834294ef
yutomizuta <yuto11752@i.softbank.jp>
2022-12-21 15:27:27 +0900

    [add]headerのSignIn,ロゴをリンク化

--- web/src/components/header.tsx
+++ web/src/components/header.tsx
@@ -1,7 +1,11 @@
+import Link from "next/link";
+
 const Header = () => {
   return (
     <header className="right-0 left-0 top-0 z-50 mx-auto flex w-full items-center justify-between bg-[#6794A3] p-4 text-[#FFFFFF] md:h-[120px] md:p-0 md:px-[60px]">
-      <h1 className="text-2xl font-bold">Soloversity</h1>
+      <Link href="/">
+        <h1 className="text-2xl font-bold">Soloversity</h1>
+      </Link>
 
       <div className="flex items-center justify-between">
         <div className="p-4">Profile</div>
@@ -10,7 +14,9 @@ const Header = () => {
         <div className="p-4">Public BBS</div>
         <div className="p-4">University BBS</div>
         <div className="p-4">Registration</div>
-        <div className="p-4">SignIn</div>
+        <Link href="/student/sign_in">
+          <a>SignIn</a>
+        </Link>
       </div>
     </header>
   );

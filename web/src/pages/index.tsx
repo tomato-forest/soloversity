f56c3f5bf547912f7f8e8f9d76843ce90ab98816
yutomizuta <yuto11752@i.softbank.jp>
2022-12-21 15:05:55 +0900

    [update]コンポーネントタグ省略

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -13,14 +13,14 @@ const Home: NextPage = () => {
         <meta name="description" content="Soloversity" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Header></Header>
+      <Header />
       <h1 className="fixed flex h-80 w-screen items-center justify-center text-6xl">
         <span>Let's spread friendship</span>
       </h1>
 
       <main className="right-0 left-0 top-0 z-50 mx-auto flex w-full items-center justify-between bg-[url('/top/background-image.jpg')] p-4 opacity-25 md:h-[460px] md:p-0 md:px-[60px]"></main>
 
-      <Footer></Footer>
+      <Footer />
     </>
   );
 };

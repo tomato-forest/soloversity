8c424bf7c9e0dca1c99b25640ed67b2385eb193a
yutomizuta <yuto11752@i.softbank.jp>
2022-12-21 13:47:53 +0900

    [update]footerをコンポーネント化

---
+++ web/src/components/footer.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+  return (
+    <footer className="right-0 left-0 top-0 z-50 mx-auto flex w-full items-center justify-between bg-[#6794A3] p-4 text-[#FFFFFF] md:h-[180px] md:p-0 md:px-[60px]">
+      <div>
+        <h1 className="p-2 text-2xl font-bold">Soloversity</h1>
+        <div className="p-1">PrivacyPolicy</div>
+        <div className="p-1">Terms of service</div>
+        <div className="p-1">Guideline</div>
+      </div>
+    </footer>
+  );
+};
+export default Footer;
--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -3,6 +3,7 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import Header from "../components/header";
+import Footer from "../components/footer";
 
 const Home: NextPage = () => {
   return (
@@ -19,14 +20,7 @@ const Home: NextPage = () => {
 
       <main className="right-0 left-0 top-0 z-50 mx-auto flex w-full items-center justify-between bg-[url('/top/background-image.jpg')] p-4 opacity-25 md:h-[460px] md:p-0 md:px-[60px]"></main>
 
-      <footer className="right-0 left-0 top-0 z-50 mx-auto flex w-full items-center justify-between bg-[#6794A3] p-4 text-[#FFFFFF] md:h-[180px] md:p-0 md:px-[60px]">
-        <div>
-          <h1 className="p-2 text-2xl font-bold">Soloversity</h1>
-          <div className="p-1">PrivacyPolicy</div>
-          <div className="p-1">Terms of service</div>
-          <div className="p-1">Guideline</div>
-        </div>
-      </footer>
+      <Footer></Footer>
     </>
   );
 };
